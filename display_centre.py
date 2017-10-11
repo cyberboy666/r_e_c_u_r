adee9c54e497ac252c6944c1c8262781c746d77a
Steven Brown <steven.brown@xero.com>
2017-10-12 12:04:30 +1300

    added terminal to tk window. Maybe.

--- display_centre.py
+++ display_centre.py
@@ -1,25 +1,28 @@
 import logging
+import math
+import os
 import sys
+import time
 import traceback
 from Tkinter import *
-import time
-import os
 
-import math
-
-import data_centre
 import video_centre
+import data_centre
 
 VIDEO_DISPLAY_TEXT = 'NOW [{}] {}              NEXT [{}] {}'
-VIDEO_DISPLAY_BANNER_LIST = ['[', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', ']']
+VIDEO_DISPLAY_BANNER_LIST = [
+    '[', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', ']']
 VIDEO_DISPLAY_BANNER_TEXT = '{} {} {}'
 
 logger = data_centre.setup_logging()
 
 tk = Tk()
-tk.withdraw()
+# tk.withdraw()
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
+
+wid = canvas.winfo_id()
+os.system('xterm -into %d -geometry 40x20 -sb &' % wid)
 
 data = data_centre.data()
 
@@ -27,6 +30,6 @@ try:
     video_driver = video_centre.video_driver(canvas)
 
     while True:
-	tk.update()
+        tk.update()
 except Exception as e:
     logger.error(str(e))

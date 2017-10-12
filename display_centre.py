de251bcab2872a2d44b68975468626ccfb25438d
Steven Brown <steven.brown@xero.com>
2017-10-12 16:23:41 +1300

    Cleaned up and added another label

--- display_centre.py
+++ display_centre.py
@@ -10,29 +10,30 @@ from Tkinter import *
 import video_centre
 import data_centre
 
-# logger = data_centre.setup_logging()
 tk = Tk()
 
-label1Text = StringVar()
-label1Text.set('blah')
-# tk.withdraw()
-frame = Frame(tk, width=500, height=400)
-# data = data_centre.data()
-label = Label(tk, textvariable=label1Text)
-video_driver = video_centre.video_driver(frame)
+label_position_value = StringVar()
+label_position_value.set('Current Position: --:--')
+label_length_value = StringVar()
+label_length_value.set('Video Length: --:--')
 
-label.pack()
+frame = Frame(tk, width=500, height=400)
+label_position = Label(tk, textvariable=label_position_value)
+label_length = Label(tk, textvariable=label_length_value)
+video_driver = video_centre.video_driver(frame)
+label_length.pack()
+label_position.pack()
 
 def key(event):
     print "pressed", repr(event.char)
     print "video position is :{}".format(video_driver.current_player.get_position())
-    if(event.char in ['0','1','2'] ):
+    if(event.char in ['0', '1', '2']):
         print 'updating next bank'
         data_centre.update_next_bank_number(int(event.char))
         #video_driver.next_player.reload_content()
 
 def update_current_time():
-    label1Text.set(convert_int_to_string_for_display(video_driver.current_player.get_position() / 1000000))
+    label_position_value.set('Current Position:' + convert_int_to_string_for_display(video_driver.current_player.get_position() / 1000000))
     tk.after(500, update_current_time)
 
 frame.bind("<Key>", key)
@@ -42,9 +43,3 @@ frame.focus_set()
 
 tk.after(500, update_current_time)
 tk.mainloop()
-# try:
-# #    video_driver = video_centre.video_driver(canvas)
-#     pass
-
-# except Exception as e:
-#     # logger.error(str(e))

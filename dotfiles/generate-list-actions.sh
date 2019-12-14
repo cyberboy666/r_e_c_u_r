eef49b0bf2e6ffaacb48e4f5c3bb30211a418b03
Tristan Rowley <doctea@gmail.com>
2019-12-14 16:47:30 +0000

    generate-list-actions script shows dynamic regex routes too

--- dotfiles/generate-list-actions.sh
+++ dotfiles/generate-list-actions.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+echo "Methods:"
 grep " def " actions.py | sed -e 's/ def //' | sed -e 's/self//' | sed -e 's/(, /(/' | sed -e 's/()//' | sort -n
+
+echo "Dynamic routes:"
+grep "r\"" actions.py | grep "\["

6c3848cf6cf33816cbeff0819edb17cccd966bbd
Tristan Rowley <doctea@gmail.com>
2019-12-14 14:19:01 +0000

    merged and sorts shaders now too

--- display_centre/menu.py
+++ display_centre/menu.py
@@ -203,7 +203,7 @@ class ShadersMenu(Menu):
         root, dirs, files = next(os.walk(current_path))
 
         indent = ' ' * 4 * (current_level)
-        for folder in dirs:
+        for folder in sorted(dirs):
             is_open, char = self._check_folder_state(folder)
             self.menu_list.append(dict(name='{}{}{}'.format(indent, folder, char), is_shader=False))
             if (is_open):
@@ -211,6 +211,7 @@ class ShadersMenu(Menu):
                 next_level = current_level + 1
                 self._add_folder_to_shaders_list(next_path, next_level)
 
+        files.sort()
         for f in files:
             split_name = os.path.splitext(f)
             if (split_name[1].lower() in ['.frag', '.shader', '.glsl', '.glslf', '.fsh']):

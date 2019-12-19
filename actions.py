5d239e5a5d4d37333a07bba805da3299e76e3124
Tristan Rowley <doctea@gmail.com>
2019-12-19 00:04:05 +0000

    fix regex

--- actions.py
+++ actions.py
@@ -892,7 +892,7 @@ class Actions(object):
                 ( r"toggle_shader_layer_([0-2])", self.toggle_shader_layer ),
                 ( r"start_shader_layer_([0-2])",  self.shaders.start_shader ),
                 ( r"stop_shader_layer_([0-2])",   self.shaders.stop_shader ),
-                ( r"set_the_shader_param_([0-2])_layer_([0-2])_continuous", self.shaders.set_param_layer_to_amount )
+                ( r"set_the_shader_param_([0-4])_layer_([0-2])_continuous", self.shaders.set_param_layer_to_amount )
         }
 
     def get_callback_for_method(self, method_name, argument):

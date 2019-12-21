efc0de2797aff582549f97920fcfe15008551a84
Tristan Rowley <doctea@gmail.com>
2019-12-21 14:52:55 +0000

    recording automation overdub works, without the weird 'ghosted parameters' bug

--- plugins/ShaderLoopRecordPlugin.py
+++ plugins/ShaderLoopRecordPlugin.py
@@ -2,17 +2,28 @@ import data_centre.plugin_collection
 from data_centre.plugin_collection import ActionsPlugin, SequencePlugin
 
 class ShaderLoopRecordPlugin(ActionsPlugin,SequencePlugin):
+    DEBUG_FRAMES = False
     def __init__(self, plugin_collection):
         super().__init__(plugin_collection)
 
         self.PRESET_FILE_NAME = "ShaderLoopRecordPlugin/frames.json"
 
         self.frames = self.load_presets()
+        self.reset_ignored()
 
     def load_presets(self):
         #try:
         print("trying load presets? %s " % self.PRESET_FILE_NAME)
-        return self.pc.read_json(self.PRESET_FILE_NAME) or self.clear_frames()
+        p = self.pc.read_json(self.PRESET_FILE_NAME)
+        if p and len(p)<(int(self.duration / self.frequency)):
+            print("adding more slots due to size change")
+            p += [None]*((int(self.duration / self.frequency))-len(p))
+            print("len is now %s" % len(p))
+            return p
+        elif p:
+            return p
+        else:
+            return self.clear_frames()
         #except:
         #    return self.clear_frames()
 
@@ -30,82 +41,126 @@ class ShaderLoopRecordPlugin(ActionsPlugin,SequencePlugin):
                 ( r"toggle_loop_automation", self.toggle_loop_automation ),
                 ( r"toggle_record_automation", self.toggle_record_automation ),
                 ( r"toggle_overdub_automation", self.toggle_overdub_automation ),
+                ( r"clear_automation", self.clear_frames ),
         ]
 
     def toggle_overdub_automation(self):
         self.overdub = not self.overdub
+        if not self.overdub:
+            self.reset_ignored()
 
     def toggle_record_automation(self):
         self.recording = not self.recording
         if self.recording and not self.overdub:
             self.clear_frames()
         if not self.recording:
+            self.reset_ignored()
             self.last_frame = None
             self.save_presets()
 
     def clear_frames(self):
         self.frames = [{}] * (int(self.duration / self.frequency))
+        if self.DEBUG_FRAMES: print ("clear_frames set to %s" % (int(self.duration / self.frequency)))
         return self.frames
 
-    duration = 5000
+    def reset_ignored(self):
+        self.ignored = { 'shader_params': [[None]*4]*3 }
+
+    duration = 2000
     frequency = 25 
     recording = False
     overdub = True
     last_frame = None
     def run_sequence(self, position):
-        current_frame_index = int(position * 100)
+        if self.DEBUG_FRAMES: print (">>>>>>>>>>>>>>frame at %s" % position)
+        current_frame_index = int(position * (int(self.duration / self.frequency)))
+        #print("got frame index %s" % current_frame_index)
 
-        current_frame = self.get_live_frame()
+        current_frame = self.get_live_frame().copy()
+        if self.DEBUG_FRAMES: print("current_frame copy before recall is %s" % current_frame['shader_params'])
 
-        if not self.recording or self.overdub:
+        if not self.recording:
+            #pass
             self.recall_frame(current_frame_index)
+        #if self.overdub:
+        #    self.recall_frame(current_frame_index,ignored = self.last_frame
         if self.recording:
             if not self.last_frame: 
                 self.last_frame = current_frame
+            if self.DEBUG_FRAMES: print("pre-diff frame is %s" % current_frame['shader_params'])
             diff = self.get_frame_diff(self.last_frame,current_frame)
+            if self.DEBUG_FRAMES: print("diffed frame is %s" % diff['shader_params'])
             if self.overdub and self.frames[current_frame_index]:
+                self.ignored = self.merge_frames(self.ignored, diff)
+                self.recall_frame(current_frame_index, ignored = self.ignored)
+                #self.ignored = self.merge_frames(self.ignored, diff)
                 diff = self.merge_frames(self.frames[current_frame_index], diff)
+                if self.DEBUG_FRAMES:  print("after diff2 is: %s" % diff['shader_params'])
             self.frames[current_frame_index] = diff #self.get_frame_diff(self.last_frame,current_frame)
             self.last_frame = self.get_live_frame()
+        if self.DEBUG_FRAMES:  print("<<<<<<<<<<<<<< frame at %s" % position)
 
     # overlay frame2 on frame1
     def merge_frames(self, frame1, frame2):
         from copy import deepcopy
         f = deepcopy(frame1) #frame1.copy()
-        """print("got frame1 %s" % frame1)
-        print("got frame2 %s" % frame2)
-        print("got f %s" % f)"""
+        if self.DEBUG_FRAMES:  print("merge_frames: got frame1 %s" % frame1)
+        if self.DEBUG_FRAMES:  print("merge_frames: got frame2 %s" % frame2)
         for i,f2 in enumerate(frame2['shader_params']):
             for i2,p in enumerate(f2):
                 if p is not None:
                     f['shader_params'][i][i2] = p
+        if self.DEBUG_FRAMES:  print("merge_frames: got f %s" % f)
         return f
 
     def get_frame_diff(self, last_frame, current_frame):
         if not last_frame: return current_frame
 
-        values = [[None]*4]*3 # 3 shader layers, 4 params
+        if self.DEBUG_FRAMES:
+            print("get_frame_diff>>>>")
+            print("last_frame: \t%s" % last_frame['shader_params'])
+            print("current_frame: \t%s" % current_frame['shader_params'])
+
+        #values = [[None]*4]*3 # 3 shader layers, 4 params
+        values = [[None]*4,[None]*4,[None]*4]
         #print (current_frame.get('shader_params'))
-        for i,n in enumerate(last_frame.get('shader_params')):
-            for i2,p in enumerate(n):
-                if p != current_frame.get('shader_params')[i][i2]:
-                    values[i][i2] = p
+        for layer,params in enumerate(last_frame.get('shader_params',[[None]*4]*3)):
+            if self.DEBUG_FRAMES:  print("got layer %s params: %s" % (layer, params))
+            for param,p in enumerate(params):
+                if p is not None and p != current_frame.get('shader_params')[layer][param]:
+                    if self.DEBUG_FRAMES: print("setting layer %s param %s to %s" % (layer,param,p))
+                    values[layer][param] = p
 
         if last_frame['feedback_active'] != current_frame['feedback_active']:
             feedback_active = current_frame['feedback_active']
         else:
             feedback_active = None
 
+        if self.DEBUG_FRAMES: print("values is\t%s " % values)
+
         diff = { 'shader_params': values, 'feedback_active': feedback_active }
+        if self.DEBUG_FRAMES:  print("returning %s\n^^^^" % diff['shader_params'])
                     
         return diff
 
 
     def get_live_frame(self):
         from plugins.ShaderQuickPresetPlugin import ShaderQuickPresetPlugin
-        return self.pc.get_plugins(ShaderQuickPresetPlugin)[0].get_live_frame()
+        live = self.pc.get_plugins(ShaderQuickPresetPlugin)[0].get_live_frame().copy()
+        if self.DEBUG_FRAMES:  print ("------\nget_live_frame got %s" % live['shader_params'])
+        return live
 
-    def recall_frame(self, index):
+    def recall_frame(self, index, ignored = None):
         from plugins.ShaderQuickPresetPlugin import ShaderQuickPresetPlugin
-        self.pc.get_plugins(ShaderQuickPresetPlugin)[0].recall_frame_params(self.frames[index])
+        if ignored is not None:
+            f = self.frames[index].copy()
+            for ix,x in enumerate(ignored['shader_params']):
+               for ip,p in enumerate(x):
+                  if p is not None:
+                      print("ignoring %ix,%ip" % (ix,ip))
+                      f['shader_params'][ix][ip] = None  
+            self.pc.get_plugins(ShaderQuickPresetPlugin)[0].recall_frame_params(f)
+        else:
+            if self.DEBUG_FRAMES:  print("recall_frame about to recall %s" % self.frames[index])
+            self.pc.get_plugins(ShaderQuickPresetPlugin)[0].recall_frame_params(self.frames[index])
 
--- plugins/ShaderQuickPresetPlugin.py
+++ plugins/ShaderQuickPresetPlugin.py
@@ -40,19 +40,23 @@ class ShaderQuickPresetPlugin(ActionsPlugin): #,SequencePlugin):
         self.store_current_preset()
 
     def get_live_frame(self):
-        return {
+        #print("get_live_frame: %s" % self.pc.message_handler.shaders.selected_param_list)
+        frame = {
                 'selected_shader_slots': [ shader.get('slot',None) for shader in self.pc.message_handler.shaders.selected_shader_list ],
                 'shader_params': copy.deepcopy(self.pc.message_handler.shaders.selected_param_list),
                 'layer_active_status': copy.deepcopy(self.pc.message_handler.shaders.selected_status_list),
                 'feedback_active': self.pc.data.feedback_active,
+                'x3_as_speed': self.pc.data.settings['shader']['X3_AS_SPEED']['value']
         }
+        #print("built frame: %s" % frame['shader_params'])
+        return frame
 
     def store_current_preset(self):
         if self.selected_preset is None: self.selected_preset = 0
 
         insert_position = self.selected_preset
         self.presets[insert_position] = self.get_live_frame()
-        print ("stored %s at position %s" % (self.presets[insert_position], insert_position))
+        #print ("stored %s at position %s" % (self.presets[insert_position], insert_position))
         self.selected_preset = insert_position
 
         self.save_presets()
@@ -73,10 +77,14 @@ class ShaderQuickPresetPlugin(ActionsPlugin): #,SequencePlugin):
         self.recall_frame(preset)
 
     def recall_frame_params(self, preset):
-        for (layer, param_list) in enumerate(preset.get('shader_params',[])):
+        if not preset:
+            return
+        #print("recall_frame_params got: %s" % preset.get('shader_params'))
+        for (layer, param_list) in enumerate(preset.get('shader_params')):
             if param_list:
                 for param,value in enumerate(param_list):
                   if value is not None:
+                    #print("recalling layer %s param %s: value %s" % (layer,param,value))
                     self.pc.midi_input.call_method_name('set_the_shader_param_%s_layer_%s_continuous' % (param,layer), value)
 
         if preset.get('feedback_active') is not None:
@@ -90,9 +98,11 @@ class ShaderQuickPresetPlugin(ActionsPlugin): #,SequencePlugin):
 
         self.recall_frame_params(preset)
 
+        self.pc.data.settings['shader']['X3_AS_SPEED']['value'] = preset.get('x3_as_speed')
+
         for (layer, slot) in enumerate(preset.get('selected_shader_slots',[])):
             if slot is not None:
-                print("setting layer %s to slot %s" % (layer, slot))
+                #print("setting layer %s to slot %s" % (layer, slot))
                 self.pc.midi_input.call_method_name('play_shader_%s_%s' % (layer, slot))
 
         for (layer, active) in enumerate(preset.get('layer_active_status',[])):

e8594e04fa3bbef1aef41f147d1f9d28b13ecd51
Tristan Rowley <doctea@gmail.com>
2019-12-22 18:22:08 +0000

    multiple automation clips

--- plugins/ShaderLoopRecordPlugin.py
+++ plugins/ShaderLoopRecordPlugin.py
@@ -2,6 +2,7 @@ import data_centre.plugin_collection
 from data_centre.plugin_collection import ActionsPlugin, SequencePlugin
 
 class ShaderLoopRecordPlugin(ActionsPlugin,SequencePlugin):
+    MAX_CLIPS = 8
 
     def __init__(self, plugin_collection):
         super().__init__(plugin_collection)
@@ -15,15 +16,21 @@ class ShaderLoopRecordPlugin(ActionsPlugin,SequencePlugin):
         #try:
         print("trying load presets? %s " % self.PRESET_FILE_NAME)
         p = self.pc.read_json(self.PRESET_FILE_NAME)
-        if p and len(p)<(int(self.duration / self.frequency)):
-            print("adding more slots due to size change")
-            p += [None]*((int(self.duration / self.frequency))-len(p))
-            print("len is now %s" % len(p))
+        if p:
+            while len(p)<self.MAX_CLIPS:
+                print ("adding clip ")
+                p += self.get_empty_clip(self.duration) #[ [None] ] #*((int(self.duration / self.frequency))-len(p)) ]
+            for i in p:
+                print("got automation clip of duration %s" % len(i))
+                if i and len(i)<(int(self.duration / self.frequency)):
+                    print("adding more slots due to size change")
+                    i += [None]*((int(self.duration / self.frequency))-len(i))
+                    print("len is now %s" % len(i))
             return p
         elif p:
             return p
         else:
-            return self.clear_frames()
+            return self.get_factory_reset()
         #except:
         #    return self.clear_frames()
 
@@ -41,7 +48,8 @@ class ShaderLoopRecordPlugin(ActionsPlugin,SequencePlugin):
                 ( r"toggle_loop_automation", self.toggle_loop_automation ),
                 ( r"toggle_record_automation", self.toggle_record_automation ),
                 ( r"toggle_overdub_automation", self.toggle_overdub_automation ),
-                ( r"clear_automation", self.clear_frames ),
+                ( r"clear_automation", self.clear_clip ),
+                ( r"select_automation_clip_([0-3])", self.select_clip ),
         ]
 
     def toggle_overdub_automation(self):
@@ -59,8 +67,16 @@ class ShaderLoopRecordPlugin(ActionsPlugin,SequencePlugin):
             self.last_saved_index = None
             self.save_presets()
 
-    def clear_frames(self):
-        self.frames = [{}] * (int(self.duration / self.frequency))
+    def get_empty_clip(self, duration = 2000):
+        return [{}] * (int(duration / self.frequency))
+
+    def get_factory_reset(self):
+        return [ self.get_empty_clip(self.duration) for i in range(self.MAX_CLIPS) ]
+
+    def clear_clip(self,clip = None):
+        if clip is None:
+            clip = self.selected_clip
+        self.frames[clip] = self.get_empty_clip(self.duration) * self.MAX_CLIPS
         self.reset_ignored()
         if self.DEBUG_FRAMES: print ("clear_frames set to %s" % (int(self.duration / self.frequency)))
         return self.frames
@@ -69,6 +85,11 @@ class ShaderLoopRecordPlugin(ActionsPlugin,SequencePlugin):
         # print("!!!!resetting ignored")
         self.ignored = { 'shader_params': [[None]*4,[None]*4,[None]*4] }
 
+    def select_clip(self, clip):
+        self.selected_clip = clip
+
+    selected_clip = 0
+
     duration = 2000
     frequency = 10 #25 
     recording = False
@@ -76,48 +97,54 @@ class ShaderLoopRecordPlugin(ActionsPlugin,SequencePlugin):
     #ignored = None # set in reset_ignored in init - used for tracking what parans have changed since overdub
     last_frame = None # for tracking what's changed between frames when overdubbing
     last_saved_index = None # for backfilling
-    DEBUG_FRAMES = False#True
+    DEBUG_FRAMES = True#True
     def run_sequence(self, position):
         current_frame_index = int(position * (int(self.duration / self.frequency)))
         if self.DEBUG_FRAMES: print (">>>>>>>>>>>>>>frame at %i%%: %i" % (position*100, current_frame_index))
         #print("got frame index %s" % current_frame_index)
 
         current_frame = self.pc.shaders.get_live_frame().copy()
-        if self.DEBUG_FRAMES: print("current_frame copy before recall is %s" % current_frame['shader_params'])
 
+        selected_clip = self.selected_clip
+        if self.DEBUG_FRAMES: print("current_frame copy before recall is %s" % current_frame['shader_params'])
+        #print ("%s clips, looks like %s" % (len(self.frames),self.frames))
+
+        #print("selected_clip is %s "%selected_clip)
+        clip = self.frames[selected_clip]
+        saved_frame = self.frames[selected_clip][current_frame_index]
         if not self.recording:
-            self.recall_frame_index(current_frame_index)
+            self.pc.shaders.recall_frame(saved_frame)
         if self.recording:
             if self.last_frame is None: 
                 self.last_frame = current_frame
-            if self.DEBUG_FRAMES: print("pre-diff frame is\t%s" % current_frame['shader_params'])
             if self.DEBUG_FRAMES: print("last frame is \t\t%s" % self.last_frame['shader_params'])
             if self.DEBUG_FRAMES: print("current f is  \t\t%s" % current_frame['shader_params'])
             diff = self.pc.shaders.get_frame_diff(self.last_frame,current_frame)
             if self.DEBUG_FRAMES: print("diffed frame is \t%s" % diff['shader_params'])
-            if self.overdub and self.frames[current_frame_index]:
+
+            if self.overdub and saved_frame:
                 # add the params tweaked this frame to the params to be ignored by recall
-                if self.DEBUG_FRAMES: print("saved frame is \t%s" % self.frames[current_frame_index]['shader_params'])
+                if self.DEBUG_FRAMES: print("saved frame is \t%s" % saved_frame['shader_params'])
                 self.ignored = self.pc.shaders.merge_frames(self.ignored, diff)
                 diff = self.pc.shaders.merge_frames(
-                        self.pc.shaders.get_frame_ignored(self.frames[current_frame_index], self.ignored),
+                        self.pc.shaders.get_frame_ignored(saved_frame, self.ignored),
                         diff
                 )
                 #diff = self.pc.shaders.merge_frames(self.pc.shaders.get_live_frame(), diff)
                 self.pc.shaders.recall_frame(diff)
                 if self.DEBUG_FRAMES:  print("after diff2 is:  \t%s" % diff['shader_params'])
             if self.DEBUG_FRAMES: print("||||saving frame \t%s" % (diff['shader_params']))
-            self.frames[current_frame_index] = diff #self.get_frame_diff(self.last_frame,current_frame)
+            self.frames[selected_clip][current_frame_index] = diff #self.get_frame_diff(self.last_frame,current_frame)
             #backfill frames
             if self.last_saved_index is not None:
                 if self.DEBUG_FRAMES: print ("last_saved_index is %s, current_frame_index is %s" % (self.last_saved_index, current_frame_index))
-                for i in range(current_frame_index - (self.last_saved_index) - 1):
-                    if self.DEBUG_FRAMES:print("backfilling frame %s" % ((self.last_saved_index+i+1)%len(self.frames)))
-                    self.frames[(self.last_saved_index+i+1)%len(self.frames)] = diff
+                for i in range(current_frame_index - (self.last_saved_index) ):
+                    if self.DEBUG_FRAMES:print("backfilling frame %s" % ((self.last_saved_index+i+1)%len(self.frames[selected_clip])))
+                    self.frames[selected_clip][(self.last_saved_index+i+1)%len(self.frames[selected_clip])] = diff
             self.last_saved_index = current_frame_index
             self.last_frame = self.pc.shaders.get_live_frame() #diff
         if self.DEBUG_FRAMES:  print("<<<<<<<<<<<<<< frame at %s" % current_frame_index)
 
-    def recall_frame_index(self, index):
-        self.pc.shaders.recall_frame_params(self.frames[index].copy())
+    """def recall_frame_index(self, index):
+        self.pc.shaders.recall_frame_params(self.frames[index].copy())"""
 

999ba330a20f8b80d9eb2332dac60b80ff3cc48e
Tristan Rowley <doctea@gmail.com>
2019-12-23 17:03:25 +0000

    very early & rough parameter mapping from osc to shader params, for use with external osc outputs

---
+++ plugins/OscSoundReactPlugin.py
@@ -0,0 +1,18 @@
+import data_centre.plugin_collection
+from data_centre.plugin_collection import ActionsPlugin#, SequencePlugin
+
+class OscSoundReact(ActionsPlugin):#,SequencePlugin):
+    def __init__(self, plugin_collection):
+        super().__init__(plugin_collection)
+        #self.disabled = True
+
+    @property
+    def parserlist(self):
+        return [
+                ( r"set_layer_([0-2])_param_([0-3])_audio_react_volume", self.set_layer_param_audio_react_volume )
+        ]
+
+    def set_layer_param_audio_react_volume(self, layer, param, value):
+        print ("set_layer_param_audio_react_volume layer %s:%s is %s" % (layer, param, value))
+        self.pc.actions.call_method_name("set_the_shader_param_%s_layer_%s_continuous"%(param,layer), value/2000.0)
+
--- user_input/osc_input.py
+++ user_input/osc_input.py
@@ -21,7 +21,7 @@ class OscInput(object):
 
     def setup_osc_server(self):
         server_parser = argparse.ArgumentParser()
-        server_parser.add_argument("--ip", default="127.0.0.1", help="the ip")
+        server_parser.add_argument("--ip", default="0.0.0.0", help="the ip")
         server_parser.add_argument("--port", type=int, default=5433, help="the port")
 
         server_args = server_parser.parse_args()
@@ -29,25 +29,36 @@ class OscInput(object):
         this_dispatcher = dispatcher.Dispatcher()
         this_dispatcher.map("/recurOsc", self.on_osc_input)
         this_dispatcher.map("/shutdown", self.exit_osc_server)
+        this_dispatcher.map("/*", self.process_command)
         
         server = osc_server.ThreadingOSCUDPServer((server_args.ip, server_args.port), this_dispatcher)
         server_thread = threading.Thread(target=server.serve_forever)
         server_thread.start()
+        print("started server on %s\n(%s)" % (server.server_address,dir(server)))
         return server
 
     def exit_osc_server(self, unused_addr, args):
         self.osc_server.shutdown()
 
-    def on_osc_input(self, unused_addr, args):
-        print("!!!!!!!!!!!!!!!!" + args)
+    def process_command(self, addr, args):
+        print("!!!!!!!!!!!!!!!!%s\t%s" %(addr,args))
+        #print ("%s" %self.osc_mappings.keys())
+        if addr in self.osc_mappings:
+            print("got a mapping for %s" % addr)
+            self.actions.call_method_name(self.osc_mappings[addr]['DEFAULT'][0], args)
+
+    def on_osc_input(self, addr, args):
+        print("!!!!!!!!!!!!!!!!" + addr + ", " + args)
         numpad = list(string.ascii_lowercase[0:19])
 
         if args in numpad:
             self.run_action_for_osc_channel(args)
         else:
             print('{} is not in keypad map'.format(args))
+            #if args in self.osc_mappings:
+            #    self
 
-    def run_action_for_osc_channel(self, channel):
+    def run_action_for_osc_channel(self, channel, args = None):
         this_mapping = self.osc_mappings[channel]
         if self.data.control_mode in this_mapping:
             mode = self.data.control_mode
@@ -57,13 +68,13 @@ class OscInput(object):
         if self.data.function_on and len(this_mapping[mode]) > 1:
             print('the action being called is {}'.format(this_mapping[mode][1]))
             #getattr(self.actions, this_mapping[mode][1])()
-            self.actions.call_method_name(this_mapping[mode][1])
+            self.actions.call_method_name(this_mapping[mode][1], args)
             if self.data.settings['sampler']['FUNC_GATED']['value'] == 'off':
                 self.data.function_on = False
         else:
             print('the action being called is {}'.format(this_mapping[mode][0]))
             #getattr(self.actions, this_mapping[mode][0])()
-            self.actions.call_method_name(this_mapping[mode][0])
+            self.actions.call_method_name(this_mapping[mode][0], args)
       
         self.display.refresh_display()
 

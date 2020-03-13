542c123bcbb177b7a038c00e9a9f18a8e54f057f
Tristan Rowley <doctea@gmail.com>
2020-03-13 00:31:20 +0000

    use tee properly

--- dotfiles/profiler.sh
+++ dotfiles/profiler.sh
@@ -2,5 +2,5 @@
 # launcher.sh
 sleep 2
 xmodmap ~/r_e_c_u_r/dotfiles/.remap &
-python3 -m cProfile ~/r_e_c_u_r/r_e_c_u_r.py | tee >profile.output
+python3 -m cProfile ~/r_e_c_u_r/r_e_c_u_r.py | tee profile.output
 
